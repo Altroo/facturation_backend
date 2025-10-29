c185a1237ffcc6917fbed953563cbb0ffe7e95bf
Youness <service-it@casadilusso.ma>
2025-10-29 09:45:54 +0100

    core: update redis dependencies and utils.py response

--- facturation_backend/utils.py
+++ facturation_backend/utils.py
@@ -1,24 +1,19 @@
 from rest_framework.views import exception_handler
 from http import HTTPStatus
-from rest_framework.views import Response
+from rest_framework.response import Response
 
 
-def api_exception_handler(exc, context) -> Response:
-    """Custom API exception handler."""
-
-    # Call REST framework's default exception handler first,
-    # to get the standard error response.
+def api_exception_handler(exc, context):
     response = exception_handler(exc, context)
 
     if response is not None:
-        # Using the description's of the HTTPStatus class as error message.
         http_code_to_message = {v.value: v.description for v in HTTPStatus}
-        error_payload = [
-            {
-                "status_code": response.status_code,
-                "message": http_code_to_message[response.status_code],
-                "details": response.data,
-            }
-        ]
-        response.data = error_payload
+        error_payload = {
+            "status_code": response.status_code,
+            "message": http_code_to_message.get(response.status_code, ""),
+            "details": response.data,
+        }
+        # Preserve the HTTP status code!
+        return Response(error_payload, status=response.status_code)
+
     return response
